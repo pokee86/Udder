2470c1fd5d5221d00f71d9d3e8ddc2a004c4cc8a
Bartek Pokorski <barciu86@gmail.com>
2021-05-17 07:29:37 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Bartek Pokorski on 17/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
